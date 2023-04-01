e8abdef96cc28d44c365850590b71af9a04ff7d9
Souphalack Ward <19916303@student.curtin.edu.au>
2023-04-01 13:37:37 +0800

    Added Teplate containter in JS

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
+var templateContainer = document.getElementById("list-item-template");
+var template = templateContainer.innerHTML
 
 // Step 2 Write the behaviour
 // Write the fuctionality 
