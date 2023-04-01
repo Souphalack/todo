5aa53646f5179d792fd03d291817da3b9571abca
Souphalack Ward <19916303@student.curtin.edu.au>
2023-04-01 14:42:37 +0800

    added ;

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
 var templateContainer = document.getElementById("list-item-template");
-var template = templateContainer.innerHTML
+var template = templateContainer.innerHTML;
 
 // Step 2 Write the behaviour
 // Write the fuctionality 
