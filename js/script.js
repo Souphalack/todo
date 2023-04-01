e12e8ec9aea78bd882cfa749dc8b0a1c2a3392e5
Souphalack Ward <19916303@student.curtin.edu.au>
2023-04-01 13:24:51 +0800

    Added the Element in JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// Empty script file to start with
+// Step 1  find the element
+var addTaskButton = document.getElementById("add-task");
+var newTaskInput = document.getElementById("task-input");
+var todoListContainer = document.getElementById("todo-list");
+
+// Step 2 Write the behaviour
+function onAddTaskClicked(event){
+    var taskName = newTaskInput.value;
+}
+// Step 3 link to event handler 
