33772f7aec3b582062cbf5c74bc7b359990a2c0c
Souphalack Ward <19916303@student.curtin.edu.au>
2023-04-01 15:59:00 +0800

    Copy new JS

--- js/script.js
+++ js/script.js
@@ -3,30 +3,32 @@
 var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
-var templateContainer = document.getElementById("list-item-template");
-var template = templateContainer.innerHTML;
+var templateElement = document.getElementById("list-item-template");
+var template = templateElement.innerHTML;
 
 // Step 2 Write the behaviour
 // Write the fuctionality 
-function onAddTaskClicked(event){
+function onAddTaskClicked(event) {
     console.log("onAddTaskClicked function called");
     var taskName = newTaskInput.value; //Copy the contens from the text box
     newTaskInput.value = ""; //, when i click the button, i want to read the content and clear it
    
     var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+
     
-    todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
+    todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
 }
 //when an Event occurs, the event contains info about the element that was clicked 
 // Take the target element, if the target element NOT contain Task - get its parent
-function onTodoListContainerClicked(event) {
+function onTodolistClicked(event) {
     var targetElement = event.target;
 
-    while (!targetElement.classList.contains("checkbox")) {
+    while (!targetElement.classList.contains("task")) {
         targetElement = targetElement.parentElement;
     }
 
-    var checkbox = targetElement.querySelector(".checkbox"); 
+    var checkbox = targetElement.querySelector(".checkbox");
+ 
 
     if (checkbox.checked) {
         targetElement.classList.add("completed");
@@ -38,4 +40,4 @@ function onTodoListContainerClicked(event) {
 // Step 3 link to event handler 
 //Link the Event to the Functionality 
 addTaskButton.addEventListener('click', onAddTaskClicked);
-todoListContainer.addEventListener('click', onTodoListContainerClicked);
+todoListContainer.addEventListener('click', onTodolistClicked);
