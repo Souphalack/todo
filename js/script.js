0a8aef8656e7fa5af215343892dee06cf2a181de
Souphalack Ward <19916303@student.curtin.edu.au>
2023-04-01 13:31:34 +0800

    Added Functionality & Event Handler in JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 // Step 1  find the element
+// Grab everything from the HTML page
 var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
 
 // Step 2 Write the behaviour
+// Write the fuctionality 
 function onAddTaskClicked(event){
-    var taskName = newTaskInput.value;
+    var taskName = newTaskInput.value; //Copy the contens from the text box
+    newTaskInput.value = ""; //, when i click the button, i want to read the content and clear it
+    todoListContainer.insertAdjacentHTML('afterbegin', taskName);
+
 }
+
 // Step 3 link to event handler 
+//Link the Event to the Functionality 
+addTaskButton.addEventListener('click', onAddTaskClicked);
