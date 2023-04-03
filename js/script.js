00662251d9077b579f571ef9f67cef1fd716d5ee
Souphalack Ward <19916303@student.curtin.edu.au>
2023-04-03 08:50:33 +0800

    Edited ! on TASK_Name

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ function renderTasks(){
     for (i = 0; i< localStorage.length; i++){
         var taskName = localStorage.key(i)
         var isCompleted = localStorage.getItem(taskName) =="true";
-        var taskHTML = template.replace("<-- TASK_NAME -->", taskName);
+        var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
         if (!isCompleted){
             todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
         }
